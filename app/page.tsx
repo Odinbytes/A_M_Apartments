70c965b5b106f43f69dd9557206151662699e786
Odinbytes <jabwunza@gmail.com>
2025-01-17 12:28:46 +0300

    installation and setup

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="">
+      <main className="">
+        <h1 className="text-airbnb-Dark">Apple Mango Apartments</h1>
+        <h2 className="text-airbnb">Book your stay with us</h2>
+      </main>
+    </div>
+  );
+}
